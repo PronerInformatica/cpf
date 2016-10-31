2348cbc8c137a98703af2e8050811f71f1e3466a
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2016-10-31 10:57:56 -0200

    Adicionado os dataProviders no PHPUnit

--- tests/CpfTest.php
+++ tests/CpfTest.php
@@ -6,27 +6,51 @@ use Proner\Cpf;
 
 class CpfTest extends \PHPUnit_Framework_TestCase
 {
-    public function test_validate()
+    public function cpfs_validate()
     {
-        $this->assertEquals(true,Cpf::validate('393.422.225-01'));
-        $this->assertEquals(true,Cpf::validate('017.935.986-06'));
-        $this->assertEquals(true,Cpf::validate('401.653.463-10'));
-
-        $this->assertEquals(true,Cpf::validate('393.422.225;01'));
-        $this->assertEquals(true,Cpf::validate('017.935.986.06'));
-        $this->assertEquals(true,Cpf::validate('401.653.463/10'));
-
-        $this->assertEquals(false,Cpf::validate('392.422.225-01'));
-        $this->assertEquals(false,Cpf::validate('018.935.986-06'));
-        $this->assertEquals(false,Cpf::validate('405.653.463-10'));
-
-        $this->assertEquals(false,Cpf::validate('392.422.225;01'));
-        $this->assertEquals(false,Cpf::validate('018.935.986.06'));
-        $this->assertEquals(false,Cpf::validate('405.653.463/10'));
+        return [
+            ['393.422.225-01', true],
+            ['017.935.986-06', true],
+            ['401.653.463-10', true],
+            ['393.422.225;01', true],
+            ['017.935.986.06', true],
+            ['401.653.463/10', true],
+            ['392.422.225-01', false],
+            ['018.935.986-06', false],
+            ['405.653.463-10', false],
+            ['392.422.225;01', false],
+            ['392.422.225;01', false],
+            ['018.935.986.06', false],
+            ['405.653.463/10', false],
+        ];
     }
 
-    public function test_registro()
+    public function cpfs_registros()
     {
-        $this->assertEquals('Rio Grande do Sul',Cpf::getUfRegister('018.935.980-06'));
+        return [
+            ['393.422.225-01', 'Bahia e Sergipe'],
+            ['017.935.986-06', 'Minas Gerais'],
+            ['401.653.463-10', 'Ceará, Maranhão e Piauí'],
+            ['393.422.225;01', 'Bahia e Sergipe'],
+            ['017.935.986.06', 'Minas Gerais'],
+            ['401.653.463/10', 'Ceará, Maranhão e Piauí'],
+        ];
+    }
+
+    /**
+     * @dataProvider cpfs_validate
+     */
+    public function test_validate($cpf, $expected)
+    {
+        $this->assertEquals($expected,Cpf::validate($cpf));
+    }
+
+
+    /**
+     * @dataProvider cpfs_registros
+     */
+    public function test_registro($cpf, $expected)
+    {
+        $this->assertEquals($expected,Cpf::getUfRegister($cpf));
     }
 }
